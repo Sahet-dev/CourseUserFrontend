02eac37c47e96e913d4b343e7ec1ee817c136d0e
Sahetmyrat <kakajansahy@gmail.com>
2024-10-27 11:16:23 +0200

    Fix design issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,14 @@ export default {
   content: [
       './index.html',
       './src/**/*.{vue,js,ts,jsx,tsx}',
+      './dist/**/*.{html,vue,js}',
+
   ],
   theme: {
     extend: {},
   },
+
+  // purge: false,
   plugins: [],
 }
 
