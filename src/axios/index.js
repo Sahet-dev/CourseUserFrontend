656e7b0e68f524685f123edb086d9ece0b3a4bd5
Sahetmyrat <kakajansahy@gmail.com>
2024-10-28 09:20:03 +0200

    trying to Fix CSRF Token issue

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,14 +1,20 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'https://course-server.sahet-dev.com/api',
+    baseURL: 'http://localhost:8000/api',
 });
 
 apiClient.interceptors.request.use(config => {
     const token = localStorage.getItem('token');
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
+    if (csrfToken) {
+        config.headers['X-CSRF-TOKEN'] = csrfToken;
+    }
+
     return config;
 });
 
