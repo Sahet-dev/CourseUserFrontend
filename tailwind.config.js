3901da8265d7e93e203c3a5678b693637cc6f3c3
Sahetmyrat <kakajansahy@gmail.com>
2024-10-28 12:53:16 +0200

    Trying to fix tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-
-  // purge: false,
-  plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
-
